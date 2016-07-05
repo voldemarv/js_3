cca282a6e152963338eb0cda63c43112daaa149a
Volodymyr <v.vovnyanka@gmail.com>
2016-07-05 16:24:47 +0000

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-var $target = document.getElementById('target');
+/*var $target = document.getElementById('target');
 $target.innerHTML='world!';
 target.innerHTML='world222!';
+*/
+/*
+var pars = document.getElementsByClassName('change')
+for (var i = 0; i <pars.length; i++)
+{
+    pars[i].innerHTML = '<b>World!</b>';
+}
+*/
+
+target.hidden = '';
